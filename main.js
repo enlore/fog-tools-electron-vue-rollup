cb879df44d1916ed7c84d2189511eb912767308f
enlore <n.e.lorenson@gmail.com>
2017-07-15 14:31:54 -0500

    Clean up electron main

--- main.js
+++ main.js
@@ -9,15 +9,11 @@ require('dotenv').config()
 
 const { resolve } = require('path')
 
-const couchbase = require('couchbase')
 const { app, BrowserWindow } = require('electron')
 
 let mainWindow
-//const winURL = process.env.NODE_ENV === 'development'
-  //? `http://localhost:${require('../../../config').port}`
-  //: `file://${__dirname}/index.html`
 
-const winUrl = resolve(__dirname, 'index.html')
+const winUrl = resolve(__dirname, 'public', 'index.html')
 
 console.info('file://' + winUrl)
 
@@ -33,18 +29,15 @@ function createWindow () {
     mainWindow = null
   })
 
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
-  //mainWindow.openDevTools()
-
-  // eslint-disable-next-line no-console
   console.log('mainWindow opened')
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+  if (process.platform !== 'darwin') { // for OSX
     app.quit()
   }
 })
