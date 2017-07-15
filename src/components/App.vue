7952d1d8205d4cf102e8f4c3cd98b606060eee65
enlore <n.e.lorenson@gmail.com>
2017-07-15 15:06:25 -0500

    Clean up

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="app">
-    <Hello></Hello>
+    <h1> HELLO GOODBYE </h1>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
 
 export default {
-  components: { Hello }
 }
 </script>
 
